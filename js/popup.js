88427d175c6d5669a60cf1d51adb8a2ac5a3a799
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-11-30 17:37:33 -0300

    add comments

--- js/popup.js
+++ js/popup.js
@@ -13,11 +13,13 @@ var hiraganaAlphabet = hiraganaAlphabet;
 var katakanaAlphabet = katakanaAlphabet;
 var wordList = wordList;
 
-var romajiToHiragana = romajiToHiragana;
+var romajiToHiragana = romajiToHiragana; // Dict that will be used for conversion
 
+// Starts app displaying text from hiragana alphabet
 var currentList = hiraganaAlphabet;
-displayText.innerHTML = currentList.rand();
+displayText.innerHTML = currentList.rand(); 
 
+// helper function, since some words from the word dataset are too large to fit the display text.
 function getTextToDisplay() {
     while (true) {
         var word = currentList.rand()
@@ -44,22 +46,21 @@ wordsTab.addEventListener('click', () => {
 
 function handleInput() {
     var input = inputField.value.toLowerCase();
-    var regex = new RegExp(Object.keys(romajiToHiragana).join("|"),"gi");
+    var regex = new RegExp(Object.keys(romajiToHiragana).join("|"),"gi"); //Regex used for conversion
 
-
+    // Since this is a Input event listener, the result is computed in real time.
     var hiraganaMatch = input.replace(regex, function(matched){
         return romajiToHiragana[matched];
     });
-
     var katakanaMatch = hiraganaMatch.replace(regex, function(matched){
         return romajiToHiragana[matched];
     });
     
+
     if (hiraganaMatch === displayText.innerHTML) {
         inputField.value = '';
         displayText.innerHTML = getTextToDisplay();
     }
-
     if (katakanaMatch === displayText.innerHTML) {
         inputField.value = '';
         displayText.innerHTML = getTextToDisplay();
